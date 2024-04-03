eecd8eabb3feb5f19b60e5c5a56b0d11926aa64e
Raehp <zhl20040514@outlook.com>
2024-04-03 15:13:09 +0800

    测试3

--- src/main/java/com/raehp/controller/TestController.java
+++ src/main/java/com/raehp/controller/TestController.java
@@ -2,6 +2,6 @@ package main.java.com.raehp.controller;
 
 public class TestController {
     public static void main(String[] args) {
-        System.out.println("自测试");
+        System.out.println("自测试2");
     }
 }
